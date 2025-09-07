0bbde02a48ebb93e8f026464d25175b48b46da09
Maksym Prysyazhny <maksymprysyazhny@icloud.com>
2025-09-07 16:06:43 -0500

    init

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,30 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
-  public int getInput() {
-    // Implement your function here
+  public static int getInput() {
+    int input; // intentionally not initialized
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("\nPlease enter a value\n");
+    input = scanner.nextInt(); // scanner not closed to fix bug.
+
+    return input;
+  }
+
+  public static boolean isValidInput(int input, int lowerBound, int upperBound) {
+    return input >= lowerBound && input <= upperBound;
   }
 
   public static void main(String[] args) {
-    // Test your function here in a driver program
+    int upperBound = 100;
+    int lowerBound = 1;
+
+    int userInput = getInput();
+    while(!isValidInput(userInput, lowerBound, upperBound)) {
+      System.out.println("Your value is invalid, please try again.");
+      userInput = getInput();
+    }
+    System.out.println("The value chosen by the user is: " + userInput+"\n");
   }
 }
